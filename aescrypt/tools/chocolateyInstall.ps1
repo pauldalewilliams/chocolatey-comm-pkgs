c43c1c914b9efa9ff9636d0b6e6f5a63a3addab8
Paul Williams <pdw@udel.edu>
2016-10-18 12:09:41 -0400

    Updated aescrypt to 3.10

---
+++ aescrypt/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'aescrypt'
+$scriptPath = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$fileType = 'msi'
+$silentArgs = '/quiet'
+$url = Join-Path $scriptPath 'AESCrypt32.msi'
+$url64 = Join-Path $scriptPath 'AESCrypt64.msi'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
